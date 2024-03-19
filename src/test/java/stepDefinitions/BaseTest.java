6ccb14ebcfe12810e6f2878055e60724fd752995
Tomas <you@example.com>
2024-03-19 02:23:43 -0300

    Refactored locators and added test cases and page objects

---
+++ src/test/java/stepDefinitions/BaseTest.java
@@ -0,0 +1,32 @@
+package stepDefinitions;
+
+
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pageObjects.*;
+
+import java.time.Duration;
+import java.util.Properties;
+
+public class BaseTest {
+
+    //Declare variables
+
+
+    public static Properties prop;
+
+
+    //Initialize page objects
+    public HomePage homePage;
+    public LoginPage loginPage;
+    public SignUpPage signUpPage;
+    public ContactUsPage contactUsPage;
+    public TestCasesPage testCasesPage;
+
+    //Declare waits
+
+
+
+}
